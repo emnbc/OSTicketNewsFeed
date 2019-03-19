28d463adc679ae861d79a5c32f1e03a768a9e4f0
Evgeny <etplaneta@mail.ru>
2019-03-19 18:33:28 +0500

    Add class

--- newsfeed.php
+++ newsfeed.php
@@ -1,23 +1,57 @@
 <?php 
 require('client.inc.php');
 require(CLIENTINC_DIR.'header.inc.php'); 
+
+class Newsfeed {
+    var $length;
+    function Newsfeed($value) {
+        $this->length = $value*3;
+    }
+    function getValue($value) {
+        $res = $value*5;
+        return $res;
+    }
+    function getNews($id) {
+        mysqli_set_charset($link = mysqli_connect( DBHOST, DBUSER, DBPASS, DBNAME ), "utf8");
+
+        if ( !$result  =  mysqli_query($link,  "SELECT * FROM newsfeed WHERE id=$id") ) {
+            echo "Произошла ошибка запроса MySQL: "  .  mysqli_error();
+        }
+
+        $news = mysqli_fetch_array($result);
+        return $news;
+    }
+    function getNewsfeed($quantity) {
+        $newsfeed = array();
+
+        mysqli_set_charset($link = mysqli_connect( DBHOST, DBUSER, DBPASS, DBNAME ), "utf8");
+
+        if ( !$result  =  mysqli_query($link,  "SELECT * FROM newsfeed ORDER BY id DESC LIMIT $quantity") ) {
+            echo "Произошла ошибка запроса MySQL: "  .  mysqli_error();
+        }
+    
+        while ( $str = mysqli_fetch_array($result) ) {
+            $newsfeed[] = $str;
+        }
+
+        return $newsfeed;
+    }
+}
+
 ?>
 
 <h1>Новости</h1>
 <hr>
 <?php 
 
-// define('DBHOST','localhost');
-// define('DBNAME','osticket');
-// define('DBUSER','osticket');
-// define('DBPASS','918611984Ost');
-
 mysqli_set_charset( $link = mysqli_connect( DBHOST, DBUSER, DBPASS, DBNAME ), "utf8" );
 if ( !$link )  die("Error MySQL");
 
-if($id = $_GET['id']) {
+if(!empty($id = $_GET['id'])) {
  
-    $news = mysqli_fetch_array(mysqli_query( $link,  "SELECT * FROM newsfeed WHERE id=$id" )); ?>
+    // $news = mysqli_fetch_array(mysqli_query( $link,  "SELECT * FROM newsfeed WHERE id=$id" )); 
+
+    $news = Newsfeed::getNews($id); ?>
 
     <h3><?php echo $news['name']; ?></h3>
     <p><?php echo $news['body']; ?></p>
@@ -28,13 +62,15 @@ if($id = $_GET['id']) {
 }
 else {
 
-    if ( !$result  =  mysqli_query( $link,  "SELECT * FROM newsfeed ORDER BY id DESC LIMIT 20" ) ) {
-        echo "Произошла ошибка запроса MySQL: "  .  mysqli_error();
-    }
+    // if ( !$result  =  mysqli_query( $link,  "SELECT * FROM newsfeed ORDER BY id DESC LIMIT 20" ) ) {
+    //     echo "Произошла ошибка запроса MySQL: "  .  mysqli_error();
+    // }
 
-    while ( $str = mysqli_fetch_array($result) ) {
-        $newsfeed[] = $str;
-    }
+    // while ( $str = mysqli_fetch_array($result) ) {
+    //     $newsfeed[] = $str;
+    // }
+
+    $newsfeed = Newsfeed::getNewsfeed(20);
 
     foreach ($newsfeed as $news) {
 ?>
@@ -47,6 +83,21 @@ else {
 <?php } } ?>
 
 <hr>
+
+<?php 
+
+$nf = new Newsfeed(13);
+
+echo $nf->length;
+
+?>
+<br />
+<?php 
+
+echo Newsfeed::getValue(5);
+
+?>
+
 <div class="clear"></div>
 
 <?php require(CLIENTINC_DIR.'footer.inc.php'); ?>
